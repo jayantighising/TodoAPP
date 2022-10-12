f6094f7f36d196ecfbd65db3ed3abd8411a2d842
Jayanti <ghisingjayanti2000@gmail.com>
2022-10-12 14:28:19 +0545

    day second of todoAPP

---
+++ resource/todo.js
@@ -0,0 +1,24 @@
+const todos= [
+    {
+        "_id":"001",
+        "title":"Complete Bootcamp Project",
+        "description":"Talk to team, make a team repository, assign task",
+    },
+    {
+        "_id":"002",
+        "title":"Trying to complete a boot camp project",
+        "description":"Talking to team, making a team repository, assigning a task",
+    },
+    {
+        "_id":"003",
+        "title":"Doing laundry",
+        "description":"Changing bedsheet",
+    },
+    {
+        "_id":"004",
+        "title":"Watching K-drama",
+        "description":"finishing in one day",
+    },
+]
+
+module.exports=todos;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var  todos= require("../resource/todo"); //doing imports form folder outside of routes.
+// console.log(todos);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jayanti' });
+  res.render('home', { todosList:todos}); // passing the value of todos variable in home page.
 });
 
-router.get('/home', function(req, res, next) {
-  res.render('home')
+// router.get('/home', function(req, res, next) {
+//   res.render('home')
+// })
+
+router.get('/add-to-do',function(req,res,next){//view file render matrai garxa.
+  res.render('addToDo',{title:"Add To Do"});// {} it sebd the value of title "Add To Do " in addToDo
+});
+
+router.post('/save-to-do',function(req,res,next){
+  todos.push({...req.body,_id:`00${todos.length}`});
+  res.redirect('/');
 })
+
+router.get('/delete-to-do/:index',function(req,res,next){ //this means dynamic index. :<> it can be changed.
+  
+  todos.splice(req.params.index,1);
+  res.redirect('/');
+}) 
+
+router.post('/edit-to-do/:id', function(req,res,next){
+  console.log(req.body, req.params);
+  const index = todos.findIndex(todo => todo._id === req.params.id);
+  todos.splice(index,1, {...req.body, _id:req.params.id});
+  res.redirect('/');
+})
+
+router.get('/open-update-form/:id', function(req,res,next){
+   //it just opens the edit page.
+   const todotodo = todos.find(todo=> todo._id === req.params.id);
+  res.render('editToDo',{todo:todotodo});
+})
+
 module.exports = router;
